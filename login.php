f39bcd64cddd36b541ed693504a75ce07f75c993
ShafiqahAlya <shafiqahalya99@gmail.com>
2026-08-20 16:43:53 +0800

    test

--- login.php
+++ login.php
@@ -162,7 +162,7 @@ if (isset($_SESSION['user_id'])) {
             </div>
 
             <h1 class="login-title">Log in</h1>
-            <p class="login-subtitle">Enter your details to access your.</p>
+            <p class="login-subtitle">Enter yourcredentials to access your workspace.</p>
 
             <form action="controllers/LoginController.php" method="POST" class="login-form">
                 
